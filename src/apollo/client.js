d5acdf4eb4b9f1607528f5d3a068515543f13e1c
USDFI <jordan@tbot.team>
2022-10-04 20:31:35 +0200

    node 16x

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/cone-exchange/cone',
+    uri: 'https://api.thegraph.com/subgraphs/name/tbotteam/stable-exchange-v2',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
