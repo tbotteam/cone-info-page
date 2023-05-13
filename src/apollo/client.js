b00357ebdbd068be21f36cff6efdc6088a649bd8
tbotteam <112426427+tbotteam@users.noreply.github.com>
2023-05-13 20:30:00 +0200

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/tbotteam/usdfi_vactory_v1',
+    uri: 'https://api.thegraph.com/subgraphs/name/tbotteam/usdfi-dexv1',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
