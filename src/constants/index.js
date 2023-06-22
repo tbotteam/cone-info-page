363047c9b579118ce25bda2876e4dfeb35451983
tbotteam <112426427+tbotteam@users.noreply.github.com>
2023-06-22 17:10:42 +0200

    new tokenlist

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,7 +20,7 @@ export const PRICE_OVERRIDES = [
   '0x55d398326f99059fF775485246999027B3197955', // USDT
   '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', // BUSDT
   '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',// USDC
-  '0x11A38e06699b238D6D9A0C7A01f3AC63a07ad318',  // USDFI
+  // '0x11A38e06699b238D6D9A0C7A01f3AC63a07ad318',  // USDFI
 ]
 
 // hide from overview list
@@ -42,6 +42,7 @@ export const UNTRACKED_COPY = 'Derived USD values may be inaccurate without liqu
 // tokens that should be tracked but arent due to lag in subgraph
 export const TRACKED_OVERRIDES = [
   '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'.toLowerCase(), // WBNB
+  '0x11A38e06699b238D6D9A0C7A01f3AC63a07ad318'.toLowerCase(),  // USDFI
   '0x2170ed0880ac9a755fd29b2688956bd959f933f8'.toLowerCase(), // WETH
   '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'.toLowerCase(), // USDC
   '0x90c97f71e18723b0cf0dfa30ee176ab653e89f40'.toLowerCase(), // FRAX
@@ -52,10 +53,10 @@ export const TRACKED_OVERRIDES = [
   // '0xA60205802E1B5C6EC1CAFA3cAcd49dFeECe05AC9'.toLowerCase(), // CONE
   // '0xe80772eaf6e2e18b651f160bc9158b2a5cafca65'.toLowerCase(), // USD+
   // '0xd7fbbf5cb43b4a902a8c994d94e821f3149441c7'.toLowerCase(), // UNKNOWN
-  // '0x1f681b1c4065057e07b95a1e5e504fb2c85f4625'.toLowerCase(), // TETU
+  '0xa3870fbBeb730BA99e4107051612af3465CA9F5e'.toLowerCase(), // STABLE
   '0x64048A7eEcF3a2F1BA9e144aAc3D7dB6e58F555e'.toLowerCase(), // FRXETH
   '0x3Cd55356433C89E50DC51aB07EE0fa0A95623D53'.toLowerCase(), // SFRXETH
   '0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5'.toLowerCase(), // HAY
   '0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275'.toLowerCase(), // BNBX
-  
+
 ]
